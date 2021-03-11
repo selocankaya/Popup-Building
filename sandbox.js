1ca6a2f30ca602d582675aec644670cae2c12a80
Selocan <sagilkaya26@gmail.com>
2021-03-11 17:36:29 +0300

    added popup proje

--- sandbox.js
+++ sandbox.js
@@ -2,13 +2,16 @@ const button = document.querySelector('button');
 const popup = document.querySelector('.popup-wrapper');
 const close = document.querySelector('.popup-close');
 
+
 button.addEventListener('click', () => {
   popup.style.display = 'block';
 });
 
+
 close.addEventListener('click', () => {
   popup.style.display = 'none';
 });
+
 
 popup.addEventListener('click', (e) => {
   if(e.target.className === 'popup-wrapper'){
